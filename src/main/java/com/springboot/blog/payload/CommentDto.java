2178d04114c4acf182569caaca08194580da1dbb
chiendvhust <107022984+chienhust123@users.noreply.github.com>
2022-10-10 16:56:25 +0700

    Initial commit

---
+++ src/main/java/com/springboot/blog/payload/CommentDto.java
@@ -0,0 +1,23 @@
+package com.springboot.blog.payload;
+
+import lombok.Data;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
+@Data
+public class CommentDto {
+    private long id;
+
+    @NotEmpty(message = "Name should not be null or empty")
+    private String name;
+
+    @NotEmpty(message = "Email should not be null or empty")
+    @Email
+    private String email;
+
+    @Size(min = 10, message = "Comment body must be minimum 10 characters")
+    @NotEmpty
+    private String body;
+}
